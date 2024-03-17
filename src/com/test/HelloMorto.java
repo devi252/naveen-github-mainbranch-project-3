5506c2c0e314a7f547c16f56a9fdc693ba02bdaa
devi252 <k.naveen252@gmail.com>
2024-03-17 13:03:03 +0530

    commit -1

--- src/com/test/HelloMorto.java
+++ src/com/test/HelloMorto.java
@@ -3,6 +3,7 @@ package com.test;
 public class HelloMorto {
 	public static void main(String[] args) {
 		System.out.println("kadsjfkladsjklf...");
+		System.out.println("push code to branch-1 from branch main");
 	}
 
 }
